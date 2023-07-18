d3cf7508e6f1f3c5dd5808f8cdaeffb33898c501
Nelson <114698224+OONelson@users.noreply.github.com>
2023-07-18 13:33:19 -0800

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-const mobileNav = document.querySelector('.mobile');
-const menu = document.querySelector('.Menu');
-
-menu.addEventListener('click', () => {
-	menu.classList.toggle('active');
-	mobileNav.classList.toggle('display');
-});
